a9de172266870e1b935d9234963eb756026514a7
ixomaxip <ixomaxip@gmail.com>
2025-07-01 17:43:10 +0200

    create engine

--- src/main.py
+++ src/main.py
@@ -9,3 +9,10 @@ class Hero(SQLModel, table=True):
     name: str = Field(index=True)
     age: int | None = Field(default=None, index=True)
     secrec_name: str
+
+
+sqlite_file_name = 'database.db'
+sqlite_url = f'sqlite://{sqlite_file_name}'
+
+connect_args = {'check_same_thread': False}
+engine = create_engine(sqlite_url, connect_args=connect_args)
