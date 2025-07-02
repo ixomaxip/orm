f4f7569bdfc5e6f3b784c402ba2ff886a55fa7a1
ixomaxip <ixomaxip@gmail.com>
2025-07-02 14:47:18 +0200

    heroes api

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ class Hero(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
     name: str = Field(index=True)
     age: int | None = Field(default=None, index=True)
-    secrec_name: str
+    secret_name: str
 
 
 sqlite_file_name = 'database.db'
@@ -45,4 +45,32 @@ def create_hero(hero: Hero, session: SessionDep) -> Hero:
 
     return hero
 
+@app.get('/heroes/')
+def read_heroes(
+    session: SessionDep,
+    offset: int = 0,
+    limit: Annotated[int, Query(le=100)] = 100,
+) -> list[Hero]:
+    heroes = session.exec(select(Hero).offset(offset).limit(limit)).all()
 
+    return heroes
+
+@app.get('/heroes/{hero_id}')
+def read_hero(hero_id: int, session: SessionDep) -> Hero:
+    hero = session.get(Hero, hero_id)
+    if not hero:
+        raise HTTPException(status_code=404, detail='Hero not found')
+
+    return hero
+
+@app.delete('heroes/{hero_id}')
+def delete_hero(hero_id: int, session: SessionDep):
+    hero = session.get(Hero, hero_id)
+
+    if not hero:
+        raise HTTPException(status_code=404, detail='Hero not found')
+    
+    session.delete(hero)
+    session.commit()
+
+    return {'ok': True}
