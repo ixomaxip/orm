27eaa88ac45db40178af6028937ea03997e64955
ixomaxip <ixomaxip@gmail.com>
2025-07-03 14:37:28 +0200

    update hero

--- src/main.py
+++ src/main.py
@@ -78,6 +78,20 @@ def read_hero(hero_id: int, session: SessionDep):
 
     return hero
 
+@app.patch('/heroes/{hero_id}', response_model=HeroPublic)
+def update_hero(hero_id: int, hero: HeroUpdate, session: SessionDep):
+    hero_db = session.get(Hero, hero_id)
+    if not hero_db:
+        raise HTTPException(status_code=404, detail='Hero not found')
+    hero_data = hero.model_dump(exclude_unset=True)
+    hero_db.sqlmodel_update(hero_data)
+
+    session.add(hero_db)
+    session.commit()
+    session.refresh(hero_db)
+
+    return hero_db
+
 @app.delete('heroes/{hero_id}')
 def delete_hero(hero_id: int, session: SessionDep):
     hero = session.get(Hero, hero_id)
